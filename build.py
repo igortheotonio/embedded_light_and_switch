c8098d82a39d86e67b11b12508195e0efa5e067d
alfredlima <alfredolms@ic.ufal.br>
2019-02-28 13:18:59 -0300

    Script para fazer o build e o flash.py

---
+++ build.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import shutil
+
+build = 'build'
+compile_code = 'cmake ..'
+make = 'make -j4'
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) != 2:
+        print('Error in parameters')
+        exit(-1)
+
+    print('Run...')
+    
+    path = sys.argv[1]
+
+    try:
+        path = os.path.join(os.getcwd(), path)
+        folder = path + build
+        if os.path.isdir(folder) :
+            shutil.rmtree(folder)
+        os.mkdir(folder)
+        os.chdir(folder)
+        os.system(compile_code)
+        os.system(make)
+    except Exception as e:
+        print('Error in compile', e)
+        pass
---
+++ flash.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+cmds = ["nrfjprog --eraseall -f nrf52", "nrfjprog --program zephyr/zephyr.hex -f nrf52", "nrfjprog --reset -f nrf52"]
+
+for cmd in cmds:
+    os.system(cmd)
