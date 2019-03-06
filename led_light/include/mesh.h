f7ea8c1bc05193031ae7bd8611080b655269d4b3
Lucas Peixoto <lpeixoto80@gmail.com>
2019-03-06 11:41:20 -0300

    Creating the main files for the firmware.

--- led_light/include/mesh.h
+++ led_light/include/mesh.h
@@ -1,8 +1,13 @@
 /**
- * @file pwm.c
+ * @file mesh.h
  * @brief
  * @author Lucas Peixoto (lucaspeixotoac@gmail.com)
  * @version 0.1
  * @date 2019-03-06
  */
+
+#ifndef _MESH_H_
+#define _MESH_H_
+
+#endif
 
--- led_light/include/node_composition.h
+++ led_light/include/node_composition.h
@@ -1,8 +1,12 @@
 /**
- * @file pwm.c
+ * @file node_composition.h
  * @brief
  * @author Lucas Peixoto (lucaspeixotoac@gmail.com)
  * @version 0.1
  * @date 2019-03-06
  */
 
+#ifndef _NODE_COMPOSITION_H_
+#define _NODE_COMPOSITION_H_
+
+#endif
