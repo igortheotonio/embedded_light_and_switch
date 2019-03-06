b8f2d8eac013b555d4624e427aba3a5b64320750
Lucas Peixoto <lpeixoto80@gmail.com>
2019-03-06 15:12:50 -0300

    Implementing the mesh, node and transition headers files.

--- led_light/include/mesh.h
+++ led_light/include/mesh.h
@@ -9,5 +9,22 @@
 #ifndef _MESH_H_
 #define _MESH_H_
 
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/mesh.h>
+#include <settings/settings.h>
+
+#include "node_composition.h"
+
+#define BT_MESH_MODEL_LIGHT_LIGHTNESS_STATUS BT_MESH_MODEL_OP_2(0x82, 0x4E)
+#define BT_MESH_MODEL_LIGHT_LIGHTNESS_LINEAR_STATUS BT_MESH_MODEL_OP_2(0x82, 0x52)
+#define BT_MESH_MODEL_LIGHT_LIGHTNESS_LAST_STATUS BT_MESH_MODEL_OP_2(0x82, 0x54)
+#define BT_MESH_MODEL_LIGHT_LIGHTNESS_DEFAULT_STATUS BT_MESH_MODEL_OP_2(0x82, 0x56)
+#define BT_MESH_MODEL_LIGHT_LIGHTNESS_RANGE_STATUS BT_MESH_MODEL_OP_2(0x82, 0x58)
+
+
+extern const struct bt_mesh_comp prov;
+
+void bt_ready(int err);
+
 #endif
 
--- led_light/include/node_composition.h
+++ led_light/include/node_composition.h
@@ -9,4 +9,42 @@
 #ifndef _NODE_COMPOSITION_H_
 #define _NODE_COMPOSITION_H_
 
+
+#include "mesh.h"
+#include "node_composition.h"
+#include "transition.h"
+
+struct light_lightness_state {
+    u16_t linear;
+    u16_t target_linear;
+
+    u16_t actual;
+    u16_t target_actual;
+
+    u16_t last;
+    u16_t def;
+
+    u8_t status_code;
+    u16_t light_range_min;
+    u16_t light_range_max;
+    u32_t lightness_range;
+
+    u8_t last_tid;
+    u16_t last_src_addr;
+    u16_t last_dst_addr;
+    s64_t last_msg_timestamp;
+
+    s32_t tt_delta_actual;
+    s32_t tt_delta_linear;
+
+    struct transition *transition;
+};
+
+extern struct light_lightness_state light_lightness_state_data;
+
+extern struct bt_mesh_model root_models[];
+extern struct bt_mesh_model node_models[];
+
+extern const struct bt_mesh_comp comp;
+
 #endif
