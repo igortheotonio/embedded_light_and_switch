cd948da20468f12888f32563529d13b4e23ca5d9
alfredlima <alfredolms@ic.ufal.br>
2019-03-07 09:17:27 -0300

    Remoção da máquina de estados

--- light_switch/src/main.c
+++ light_switch/src/main.c
@@ -2,13 +2,10 @@
 #include <misc/printk.h>
 #include <zephyr.h>
 
-#include "state_machine.h"
-
 int main(void)
 {
     while (1) {
         printk("Run...: ");
-        transition_machine(timeout);
         k_sleep(K_SECONDS(1));
     }
     return 0;
